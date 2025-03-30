67e74566a2ad48a4c54258783e5fb8698419197f
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2025-03-30 14:20:32 +0300

    u[date users api (client)

--- client/features/user/api.ts
+++ client/features/user/api.ts
@@ -13,38 +13,6 @@ type EditUserParams = {
   avatar?: File;
 };
 
-// export const getUser = async (params: GetUserParams) => {
-//   try {
-//     const res = await instance.get(`/users/${params.id}`);
-//     return res.data;
-//   } catch (err) {
-//     if (axios.isAxiosError(err)) {
-//       return err.response?.data;
-//     }
-//   }
-// };
-
-// export const editUser = async (params: EditUserParams) => {
-//   const { id, ...data } = params;
-
-//   const formData = new FormData();
-
-//   Object.entries(data).forEach(([k, v]) => formData.append(k, v));
-
-//   try {
-//     const res = await instance.patch(`/users/${id}`, formData, {
-//       headers: {
-//         'Content-Type': 'multipart/form-data',
-//       },
-//     });
-//     return res.data;
-//   } catch (err) {
-//     if (axios.isAxiosError(err)) {
-//       return err.response?.data;
-//     }
-//   }
-// };
-
 export class UsersApi {
   static async getUser(params: GetUserParams) {
     try {
